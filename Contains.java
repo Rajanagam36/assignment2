ef7dc9e307fa1e5abe84bacdf324c5c7d540bc22
Rajanagam36 <114391004+Rajanagam36@users.noreply.github.com>
2023-02-07 16:11:17 +0530

    4

---
+++ Contains.java
@@ -0,0 +1,10 @@
+public class Contains {
+    public static void main(String[] args) {
+        String str = "Hello Javatpoint readers";
+        boolean isContains = str.contains("Javatpoint");
+        System.out.println(isContains);
+        // Case Sensitive
+        System.out.println(str.contains("javatpoint")); // false
+    }
+}
+
